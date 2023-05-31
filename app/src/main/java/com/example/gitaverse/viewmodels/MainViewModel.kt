4e35e0335831cb59c17010d44150d21fbb60819e
Aditya Sood <adityasood314@gmail.com>
2023-05-31 13:57:26 +0530

    Created Viewmodels

---
+++ app/src/main/java/com/example/gitaverse/viewmodels/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.gitaverse.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.gitaverse.models.RandomShloka
+import com.example.gitaverse.repository.ShlokRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(private val shlokRepository: ShlokRepository):ViewModel() {
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            shlokRepository.getRandomShlok()
+        }
+    }
+
+    val shlok:LiveData<RandomShloka>
+    get() = shlokRepository.randomShloka
+}
---
+++ app/src/main/java/com/example/gitaverse/viewmodels/MainViewModelFactory.kt
@@ -0,0 +1,12 @@
+package com.example.gitaverse.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.gitaverse.repository.ShlokRepository
+
+class MainViewModelFactory(private val shlokRepository: ShlokRepository):ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return  MainViewModel(shlokRepository) as T
+
+    }
+}
